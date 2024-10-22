90048276598c27dd8a387ed07b963657ebb91cee
jojupacr5 <joseph.paucar@gmail.com>
2024-10-22 18:30:25 -0500

    Primer commit del backend para nuestro proyecto de Calendar

---
+++ controllers/auth.js
@@ -0,0 +1,46 @@
+const {response} = require('express');
+
+const createUser = (req, res = response) => {
+
+  const { name, email, password } = req.body;
+
+  if ( name.length < 5 ) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'El nombre debe ser de 5 letras'
+    })
+  }
+
+  res.json({
+    ok: true,
+    msg: 'registro',
+    name, 
+    email,
+    password
+  })
+}
+
+const loginUser = (req, res = response) => {
+
+  const { email, password } = req.body;
+
+  res.json({
+    ok: true,
+    msg: 'login',
+    email,
+    password
+  })
+}
+
+const revalidateToken = (req, res = response) => {
+  res.json({
+    ok: true,
+    msg: 'renew'
+  })
+}
+
+module.exports = {
+  createUser,
+  loginUser,
+  revalidateToken
+}
