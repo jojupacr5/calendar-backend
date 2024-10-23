7c64327439cc15fa5f8615687a53dc6727cf27d6
jojupacr5 <joseph.paucar@gmail.com>
2024-10-22 23:38:31 -0500

    Se creo el modelo User y un nuevo usuario desde postman haceia mongo cloud

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,28 @@
 const {response} = require('express');
+const User = require('../models/User');
 
 
-const createUser = (req, res = response) => {
+const createUser = async (req, res = response) => {
 
-  const { name, email, password } = req.body;
+  // const { name, email, password } = req.body;
+  try {
+    const user = new User( req.body );
 
-  res.status(201).json({
-    ok: true,
-    msg: 'registro',
-    name, 
-    email,
-    password
-  })
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: 'registro',
+    })
+  } catch (error) {
+    
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    })
+  }
+  
 }
 
 const loginUser = (req, res = response) => {
