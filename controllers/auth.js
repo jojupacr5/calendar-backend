2cc570e04ff351cc5a13a6f4fe808b0ab7b2b1c5
jojupacr5 <joseph.paucar@gmail.com>
2024-10-23 13:13:31 -0500

    Se genero el login de usuario y sus validaciones con la bd

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,16 +45,48 @@ const createUser = async (req, res = response) => {
   
 }
 
-const loginUser = (req, res = response) => {
+const loginUser = async (req, res = response) => {
 
   const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password
-  })
+  try {
+
+    //* 1 Validando si el correo del usuario ya existe en la bd
+    const user = await User.findOne({ email });
+    
+    if ( !user ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'No existe un usuario con ese correo'
+      });
+    }
+    
+    //* 2 Confirmar las contraseñas
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if( !validPassword ) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Contraseña incorrecta'
+      });
+    }
+
+    //* 3 Generar JWT
+    
+    res.json({
+      ok: true,
+      uid: user.id,
+      name: user.name
+    })
+
+  } catch (error) {
+
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    })
+  }
 }
 
 const revalidateToken = (req, res = response) => {
